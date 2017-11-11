d838b603c7001e85b0efb100b871277dc5877d2e
KartikSD <ksdeshamukh@gmail.com>
2017-11-11 18:20:41 +0530

    Mock repository and facade

--- src/test/scala/com/reactore/feature/CountryServiceTestSpec.scala
+++ src/test/scala/com/reactore/feature/CountryServiceTestSpec.scala
@@ -1,13 +1,23 @@
 package com.reactore.feature
 
 import org.scalatest.{Matchers, WordSpec}
+import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
 
 /**
   * created by Kartik on 11-11-2017
   */
 class CountryServiceTestSpec extends WordSpec with ScalaFutures with Matchers {
+   "Country Service " should {
+      //test cases for getCountryById
+      "return a country in get by id for id as 1" in {
+         when(MockCountryService.countryRepository.countryFuture).thenReturn(MockCountryRepository.countryFuture)
+         val expectedResult = MockCountryRepository.country1
+         val result = MockCountryService.getCountryById(1)
+         result.futureValue shouldBe expectedResult
 
+      }
+   }
 }
 
-object MockCompanyService extends CompanyService with MockCompanyFacade
+object MockCountryService extends CountryService with MockCountryFacade
